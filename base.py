68e4e58cd04baeeb2c65610c4cb40476811e6ef1
zaneway1108@gmail.com <zaneway1108@gmail.com>
2024-12-17 22:24:02 +0800

    update detail

--- base.py
+++ base.py
@@ -1,5 +1,8 @@
 # you should exec 'pip install langchain_ollama'
-# you can install ollama in pc of yourself
+# you can install ollama in pc of yourself,'https://ollama.com/'
+# more information in https://python.langchain.com/api_reference/ollama/chat_models/langchain_ollama.chat_models.ChatOllama.html
+#
+
 
 from langchain_ollama import OllamaLLM
 model = OllamaLLM(model="llama3.1:8b")
